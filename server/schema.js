f7e75660f6985261a6cbb04aa6fb657304358912
amityt <amitkumar.das@mayadata.io>
2020-06-21 22:48:19 +0530

    Added GraphQL Server
    Signed-off-by: amityt <amitkumar.das@mayadata.io>

---
+++ server/schema.js
@@ -0,0 +1,80 @@
+const {
+	GraphQLObjectType,
+	GraphQLInt,
+	GraphQLString,
+	GraphQLList,
+	GraphQLSchema,
+} = require("graphql");
+const axios = require("axios");
+
+const AllData = new GraphQLObjectType({
+	name: "AllData",
+	fields: () => ({
+		Global: {
+			type: GlobalType,
+		},
+		Countries: {
+			type: GraphQLList(CountryType),
+		},
+		Date: {
+			type: GraphQLString,
+		},
+	}),
+});
+
+const GlobalType = new GraphQLObjectType({
+	name: "Global",
+	fields: () => ({
+		NewConfirmed: { type: GraphQLInt },
+		TotalConfirmed: { type: GraphQLInt },
+		NewRecovered: { type: GraphQLInt },
+		TotalConfirmed: { type: GraphQLInt },
+		NewDeaths: { type: GraphQLInt },
+		TotalDeaths: { type: GraphQLInt },
+	}),
+});
+
+const CountryType = new GraphQLObjectType({
+	name: "Countries",
+	fields: () => ({
+		Country: { type: GraphQLString },
+		CountryCode: { type: GraphQLString },
+	}),
+});
+
+const CountryData = new GraphQLObjectType({
+	name: "Country",
+	fields: () => ({
+		country: { type: GraphQLString },
+		cases: { type: GraphQLInt },
+	}),
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: "RootQueryType",
+	fields: {
+		global: {
+			type: AllData,
+			resolve(parent, args) {
+				return axios
+					.get("https://api.covid19api.com/summary")
+					.then((res) => res.data);
+			},
+		},
+		country: {
+			type: CountryData,
+			args: {
+				country: { type: GraphQLString },
+			},
+			resolve(parent, args) {
+				return axios
+					.get(`https://corona.lmao.ninja/v2/countries/${args.country}`)
+					.then((res) => res.data);
+			},
+		},
+	},
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery,
+});
